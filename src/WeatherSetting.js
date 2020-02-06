843b523b4dccdf541f85cf63ae84912653a49b24
may7243 <49479234+may7243@users.noreply.github.com>
2020-02-06 12:04:36 +0800

    Finish downloading original codes on CodeSandbox

---
+++ src/WeatherSetting.js
@@ -0,0 +1,186 @@
+import React, { useRef } from "react";
+import styled from "@emotion/styled";
+
+import { availableLocations } from "./utils";
+
+const WeatherSettingWrapper = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
+  box-sizing: border-box;
+  padding: 20px;
+`;
+
+const Title = styled.div`
+  font-size: 28px;
+  color: ${({ theme }) => theme.titleColor};
+  margin-bottom: 30px;
+`;
+
+const StyledLabel = styled.label`
+  display: block;
+  font-size: 16px;
+  color: ${({ theme }) => theme.textColor};
+  margin-bottom: 15px;
+`;
+
+const StyledInputList = styled.input`
+  display: block;
+  box-sizing: border-box;
+  background: transparent;
+  border: 1px solid ${({ theme }) => theme.textColor};
+  outline: none;
+  width: 100%;
+  max-width: 100%;
+  min-height: 40px;
+  color: ${({ theme }) => theme.textColor};
+  font-size: 16px;
+  padding: 7px 10px;
+  margin-bottom: 40px;
+`;
+
+const StyledSelect = styled.select`
+  display: block;
+  box-sizing: border-box;
+  background: transparent;
+  border: 1px solid ${({ theme }) => theme.textColor};
+  outline: none;
+  width: 100%;
+  max-width: 100%;
+  min-height: 40px;
+  color: ${({ theme }) => theme.textColor};
+  font-size: 16px;
+  padding: 7px 10px;
+  margin-bottom: 40px;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    white-space: nowrap;
+    user-select: none;
+    margin: 0;
+    letter-spacing: 0.3px;
+    line-height: 1;
+    cursor: pointer;
+    overflow: visible;
+    text-transform: none;
+    border: 1px solid transparent;
+    background-color: transparent;
+    height: 35px;
+    width: 80px;
+    border-radius: 5px;
+
+    &:focus,
+    &.focus {
+      outline: 0;
+      box-shadow: none;
+    }
+
+    &::-moz-focus-inner {
+      padding: 0;
+      border-style: none;
+    }
+  }
+`;
+
+const Back = styled.button`
+  && {
+    color: ${({ theme }) => theme.textColor};
+    border-color: ${({ theme }) => theme.textColor};
+  }
+`;
+
+const Save = styled.button`
+  && {
+    color: white;
+    background-color: #40a9f3;
+  }
+`;
+
+const locations = availableLocations.map(location => location.cityName);
+
+const styles = [
+  {
+    name: "淺色",
+    value: "light"
+  },
+  {
+    name: "深色",
+    value: "dark"
+  }
+];
+
+const WeatherSetting = props => {
+  const {
+    setCurrentPage,
+    cityName,
+    setCurrentCity,
+    currentTheme,
+    setCurrentTheme
+  } = props;
+
+  const inputLocationRef = useRef(null);
+  const themeStyleRef = useRef(null);
+
+  const handleSave = () => {
+    const locationName = inputLocationRef.current.value;
+    const themeStyle = themeStyleRef.current.value;
+
+    if (locations.includes(locationName)) {
+      console.log(`Location: ${locationName}`);
+      console.log(`Theme: ${themeStyle}`);
+      setCurrentCity(locationName);
+      setCurrentTheme(themeStyle);
+      setCurrentPage("WeatherCard");
+    } else {
+      alert(`Error! "${locationName}" isn't available!`);
+    }
+  };
+
+  return (
+    <WeatherSettingWrapper>
+      <Title>設定</Title>
+      <StyledLabel htmlFor="location">地區</StyledLabel>
+      <StyledInputList
+        list="location-list"
+        id="location"
+        name="location"
+        ref={inputLocationRef}
+        defaultValue={cityName}
+      />
+      <datalist id="location-list">
+        {locations.map(location => (
+          <option value={location} key={location} />
+        ))}
+      </datalist>
+      <StyledLabel htmlFor="style">外觀</StyledLabel>
+      <StyledSelect
+        list="style-list"
+        id="style"
+        name="style"
+        ref={themeStyleRef}
+        defaultValue={currentTheme}
+      >
+        {styles.map(style => (
+          <option value={style.value} key={style.value}>
+            {style.name}
+          </option>
+        ))}
+      </StyledSelect>
+      <ButtonGroup>
+        <Back onClick={() => setCurrentPage("WeatherCard")}>返回</Back>
+        <Save onClick={handleSave}>儲存</Save>
+      </ButtonGroup>
+    </WeatherSettingWrapper>
+  );
+};
+
+export default WeatherSetting;
